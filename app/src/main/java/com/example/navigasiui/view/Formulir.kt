aaa2f5222be2f463c896fb7081317df16632601d
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-07 15:09:23 +0700

    Menambahkan TopAppBar pada Formulir.kt

--- app/src/main/java/com/example/navigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiui/view/Formulir.kt
@@ -2,7 +2,16 @@ package com.example.navigasiui.view
 
 import androidx.annotation.OptIn
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.material3.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -10,5 +19,14 @@ fun FormIsian(
     jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
     OnSubmitBtnClick: () ->  Unit
 ){
-
+    Scaffold (modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = {Text(stringResource(id = R.string.home),
+                    color = Color.White)},
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    colorResource(id = R.color.teal_700))
+            )}
+    ){
+    }
 }
