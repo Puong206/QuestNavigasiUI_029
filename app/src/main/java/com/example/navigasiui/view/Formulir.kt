221106a611e3b1871e57167a1eb66d76e3117260
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-07 15:11:30 +0700

    Menambahkan Column pada Formulir.kt

--- app/src/main/java/com/example/navigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiui/view/Formulir.kt
@@ -1,6 +1,9 @@
 package com.example.navigasiui.view
 
 import androidx.annotation.OptIn
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.material3.R
+import androidx.compose.ui.Alignment
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -20,13 +24,18 @@ fun FormIsian(
     OnSubmitBtnClick: () ->  Unit
 ){
     Scaffold (modifier = Modifier,
-        topBar = {
+        {
             TopAppBar(
                 title = {Text(stringResource(id = R.string.home),
                     color = Color.White)},
                 colors = TopAppBarDefaults.mediumTopAppBarColors(
                     colorResource(id = R.color.teal_700))
             )}
-    ){
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+
+        }
     }
 }
