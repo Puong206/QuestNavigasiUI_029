a622f208e31325865bf0fe22d8667e8e38e46b4a
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-07 16:06:03 +0700

    Membuat Column di dalam TampilData.kt

--- app/src/main/java/com/example/navigasiui/view/TampilData.kt
+++ app/src/main/java/com/example/navigasiui/view/TampilData.kt
@@ -1,5 +1,8 @@
 package com.example.navigasiui.view
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -30,6 +33,10 @@ fun TampilData(
                 colors = TopAppBarDefaults.mediumTopAppBarColors(colorResource(id=R.color.teal_700))
             )
         }) {
+        isiRuang->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.SpaceBetween) {
 
+        }
     }
 }
