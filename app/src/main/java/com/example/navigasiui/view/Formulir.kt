dabefee76fb961f3b634571454ddfe2002f0076d
Arya Bagas Saputra <arya.bagas.ft23@mail.umy.ac.id>
2025-11-07 14:59:26 +0700

    Membuat fungsi FormIsian

--- app/src/main/java/com/example/navigasiui/view/Formulir.kt
+++ app/src/main/java/com/example/navigasiui/view/Formulir.kt
@@ -1,2 +1,14 @@
 package com.example.navigasiui.view
 
+import androidx.annotation.OptIn
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.runtime.Composable
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormIsian(
+    jenisK:List<String> = listOf("Laki-laki", "Perempuan"),
+    OnSubmitBtnClick: () ->  Unit
+){
+
+}
